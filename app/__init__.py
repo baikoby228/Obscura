d96222f1555f20214d1c43f06fe473dd69728c47
baikoby228 <baikoby228@gmail.com>
2026-09-17 11:20:59 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from .account_manager import list_accounts, get_account, add_account, remove_account
 from .driver import get_driver
-from .noise_generator import start_noise_for_account
+from .noise_generator import start_noise_for_account, noise_worker
 from .query_generator import get_query
 
-__all__ = ['list_accounts', 'get_account', 'add_account', 'remove_account', 'get_driver', 'start_noise_for_account', 'get_query']
+__all__ = [
+    'list_accounts', 'get_account', 'add_account', 'remove_account',
+    'get_driver',
+    'start_noise_for_account', 'noise_worker',
+    'get_query'
+]
